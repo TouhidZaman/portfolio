a65045477b25713319c0b502b4fe97e47995754d
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-06-10 23:36:12 +0600

    mobile toggle sidenav added

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideNav.css";
 import profile from "../../images/Touhid.jpg";
 import { Link } from "react-router-dom";
 
 const SideNav = () => {
+    const [toggleClass, setToggleClass] = useState("");
+    const [menuClass, setMenuClass] = useState("fa-bars");
+    const toggleSideNav = () => {
+        setToggleClass((prevClass) => {
+            if (prevClass === "toggle") {
+                return "";
+            } else {
+                return "toggle";
+            }
+        });
+        setMenuClass((prevClass) => {
+            if (prevClass === "fa-bars") {
+                return "fa-times";
+            } else {
+                return "fa-bars";
+            }
+        });
+    };
     return (
-        <header>
+        <header className={`${toggleClass}`}>
             <div className="user">
                 <img src={profile} alt="" />
                 <h3 className="name">Muhammad Touhiduzzaman</h3>
@@ -31,7 +49,7 @@ const SideNav = () => {
                     </li>
                 </ul>
             </nav>
-            <div id="menu" className="fas fa-bars"></div>
+            <div id="menu" onClick={toggleSideNav} className={`fas ${menuClass}`}></div>
         </header>
     );
 };
